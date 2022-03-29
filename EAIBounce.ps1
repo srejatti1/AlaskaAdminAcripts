11b1f49afc2e1b407ff178c0415a22664ac60f57
Sreedhar Jatti <86109642+srejatti1@users.noreply.github.com>
2022-03-29 08:07:27 -0700

    Add files via upload

---
+++ EAIBounce.ps1
@@ -0,0 +1,48 @@
+# DEVELOPER -- SHANTAN POLEPALLY
+# EMAIL -- SHANTAN.POLEPALLY@ALASKAAIR.COM
+# THIS SCRIPT IS USED TO PERFORM THE FOLLOWING OPERATIONS
+#1. Bounce EAI Component
+
+try
+	{
+	
+		$myDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+		# Load config file
+		[xml]$ConfigFile = Get-Content "$MyDir\SiebelDevTasks.config"
+        $FldrNameTime = (Get-Date -format MMddyyyy) + "_" + (Get-Date -format mmss)
+		$ScriptName = "EAIBounce_"
+		$ScriptLogPath = $ConfigFile.Settings.Siebel.ScriptLogPath
+		$Gateway = $ConfigFile.Settings.Siebel.Gateway
+		$Enterprise = $ConfigFile.Settings.Siebel.Enterprise
+		$AdminUser  = $ConfigFile.Settings.Siebel.AdminUser
+		$AdminPassword=$ConfigFile.Settings.Siebel.AdminPassword
+		Start-Transcript -Path $ScriptLogPath\$ScriptName$FldrNameTime.txt
+		Set-Location -Path "D:\sea\ses\siebsrvr\BIN"
+		.\srvrmgr.exe /g $Gateway /e $Enterprise /u $AdminUser /p $AdminPassword /i "D:\FTPRoot\Rollouts\BatchScripts\PowerShell_Files\restart_eai.txt" 
+    	Set-Location -Path "D:\FTPRoot\Rollouts\BatchScripts\PowerShell_Files"
+		Write-Output "COMPLETED EXECUTING EAIBounce.PS1"
+			
+	}
+
+
+# used to catch any exception and occured in try block and send an email to the admin
+Catch
+	{
+		$ErrorMessage = $_.Exception.Message
+		$FailedItem = $_.Exception.ItemName
+		Write-Output "EXCEPTION RUNNING EAIBounce.ps1" "ERROR MESSAGE: $ErrorMessage ` FAILEDITEM: $FailedItem `n"
+	
+	}
+
+finally
+	{
+	Clear-Variable FldrNameTime
+	Clear-Variable ScriptName
+	Clear-Variable ScriptLogPath
+	Clear-Variable Gateway
+	Clear-Variable Enterprise
+	Clear-Variable AdminUser
+	Clear-Variable AdminPassword
+	Stop-Transcript
+	}
+
